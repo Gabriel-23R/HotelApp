b6c177f5f30437a33f896a1ce61e8a769dc250ee
Gabriel Ramos <ggrmramos@gmail.com>
2026-07-02 18:08:49 -0500

    Se corrige la navegación del botón Volver en Habitaciones

--- app/src/main/java/com/example/hotelapp/ui/HabitacionesActivity.java
+++ app/src/main/java/com/example/hotelapp/ui/HabitacionesActivity.java
@@ -1,6 +1,8 @@
 package com.example.hotelapp.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +16,14 @@ public class HabitacionesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_habitaciones);
+
+        // Botón Volver
+        Button btnVolver = findViewById(R.id.btnVolver);
+
+        btnVolver.setOnClickListener(v -> {
+            Intent intent = new Intent(HabitacionesActivity.this, MenuActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
